ffd7b05a36bc1db9a34fd9afc15d6f759fd14064
Pathri Vidya Praveen <vidyapraveen.2006@gmail.com>
2025-07-07 18:18:45 +0530

    Add files via upload

--- Authentication_Authorization_API/main.go
+++ Authentication_Authorization_API/main.go
@@ -1,1 +1,25 @@
+package main
 
+import (
+	"Authentication_Authorization_API/controllers"
+	"Authentication_Authorization_API/initializers"
+	"Authentication_Authorization_API/middlewares"
+
+	"github.com/gin-gonic/gin"
+)
+
+func init() {
+	initializers.LoadEnvs()
+	initializers.ConnectDB()
+
+}
+
+func main() {
+	router := gin.Default()
+
+	router.POST("/auth/signup", controllers.CreateUser)
+	router.POST("/auth/login", controllers.Login)
+	router.GET("/user/profile", middlewares.CheckAuth, controllers.GetUserProfile)
+
+	router.Run()
+}
