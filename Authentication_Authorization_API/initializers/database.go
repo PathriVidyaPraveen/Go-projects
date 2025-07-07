77cc5864a8ae145a19a3ac6fa5650499eb9fd65e
Pathri Vidya Praveen <vidyapraveen.2006@gmail.com>
2025-07-07 18:23:22 +0530

    Add files via upload

--- Authentication_Authorization_API/initializers/database.go
+++ Authentication_Authorization_API/initializers/database.go
@@ -1,1 +1,22 @@
+package initializers
 
+import (
+	"log"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDB() {
+	dsn := os.Getenv("DB_URL")
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Failed to connect to DB:", err)
+	}
+
+}
